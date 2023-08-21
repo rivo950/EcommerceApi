e3001212e67129da66729fb43853eb46e9b46113
Rivo RAFIDIMANANA <rafidimananarivo@gmail.com>
2023-08-21 13:04:44 +0300

    Create route for the 2models

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/product', ProductController::class);
+Route::apiResource('/products', ProductController::class);
+
+Route::group(['prefix'=>'products',function(){
+    Route::apiResource('/reviews/{products}',ReviewController::class);
+}]);
